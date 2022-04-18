a082fefb1c26f0e925a20b7e1c89317e4ecbe3d2
Rattana548 <68465498+Rattana548@users.noreply.github.com>
2022-04-18 20:48:00 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect('{host}', '{user}', '{password}', '{db_name}');
+$link = mysqli_connect('localhost', 'root', 'VCOgaa28330', 'shop');
 //if connection is not successful you will see text error
 if (!$link) {
        die('Could not connect: ' . mysql_error());
